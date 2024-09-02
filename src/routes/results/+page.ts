068b05c1188789da7b591c17bb651d75173b218f
Rafal <rs.szczerba@hotmail.com>
2024-09-02 22:50:43 +0200

    Fixed data flow between store and result page

--- src/routes/results/+page.ts
+++ src/routes/results/+page.ts
@@ -1,49 +1,18 @@
-import type { Result } from '$lib/models/Result';
+import { goto } from '$app/navigation';
 import { apiResult } from '$lib/store';
 import type { PageLoad } from './$types';
+import { browser } from '$app/environment';
 
-export const load: PageLoad = ({ params }) => {
-	apiResult.subscribe((value) => {
-	    console.log(value);
-	});
-	return {
-		result: <Result[]>[
-			{
-				search: {
-					id_type: 'REGON',
-					id_value: '987654321',
-					bank_account: '44 4444 4444 4444 4444 4444 4444'
-				},
-				company: {
-					bank_accounts: [],
-					name: 'Ostatnia firma',
-					address: 'Kwiatowa 2, 11-111 Kraków'
-				}
-			},
-            {
-				search: {
-					id_type: 'REGON',
-					id_value: '987654321',
-					bank_account: '44 4444 4444 4444 4444 4444 4444'
-				},
-				company: {
-					bank_accounts: ['44 4444 4444 4444 4444 4444 4444'],
-					name: 'Ostatnia firma',
-					address: 'Kwiatowa 2, 11-111 Kraków'
-				}
-			},
-            {
-				search: {
-					id_type: 'REGON',
-					id_value: '987654321',
-					bank_account: '44 4444 4444 4444 4444 4444 4442'
-				},
-				company: {
-					bank_accounts: ['44 4444 4444 4444 4444 4444 4444'],
-					name: 'Ostatnia firma',
-					address: 'Kwiatowa 2, 11-111 Kraków'
-				}
-			}
-		]
-	};
+export const load: PageLoad = async ({ params }) => {
+    const result = await new Promise((resolve) => {
+        const unsubscribe = apiResult.subscribe((value) => {
+            if (value.length == 0 && browser) {
+                goto('/');
+            }
+            resolve(value);
+            unsubscribe();
+        });
+    });
+
+    return { result };
 };
