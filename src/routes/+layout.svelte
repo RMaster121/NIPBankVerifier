b4e25abaf5ef184dac639f5e52de49046ae4e0a8
Rafal <rs.szczerba@hotmail.com>
2024-09-03 00:45:33 +0200

    Small change - moved file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.css';
-	import Footer from '$lib/Generic/Footer.svelte';
+	import Footer from '$lib/elements/Footer.svelte';
 </script>
 
 <main class="h-screen flex flex-col justify-between">
