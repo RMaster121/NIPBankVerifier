4080a7887d58ce43d16a4cab7f8fc87e7b0843b0
Rafal <rs.szczerba@hotmail.com>
2024-09-04 01:16:09 +0200

    Fixed formating

--- src/lib/utilities/call_api.ts
+++ src/lib/utilities/call_api.ts
@@ -1,7 +1,16 @@
 export async function call_api(id_type: string, id_value: string): Promise<Response> {
-    const currentDate = new Date().toLocaleString().split(', ')[0].split('.').reverse().join('-');
-	let response = await fetch(`https://wl-api.mf.gov.pl/api/search/${id_type.toLowerCase}/${id_value}?date=${currentDate}`, {
-		method: 'GET'
-	});
-    return response;
+	const currentDate = new Date()
+		.toLocaleString()
+		.split(', ')[0]
+		.split('.')
+		.reverse()
+		.map((part) => (part.length === 1 ? '0' + part : part))
+		.join('-');
+	const response = await fetch(
+		`https://wl-api.mf.gov.pl/api/search/${id_type.toLowerCase()}/${id_value}?date=${currentDate}`,
+		{
+			method: 'GET'
+		}
+	);
+	return response;
 }
