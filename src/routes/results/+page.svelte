7252ae709dacafea3e49d186dea10d0e4745f765
Rafal <rs.szczerba@hotmail.com>
2024-09-04 21:55:24 +0200

    Added back button

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -49,8 +49,29 @@
 
 <div class="container mx-auto p-4">
 	<div class="bg-white shadow-md rounded-lg">
-		<h1 class="text-xl font-bold p-4">Wyniki wyszukiwania</h1>
-		<div class="space-y-4 p-4">
+		<div class="flex items-center p-4">
+			<button
+				class="mr-2"
+				on:click={() => history.back()}
+				aria-label="Back"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6 text-blue-500 hover:text-blue-700"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					stroke-width="2"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M15 19l-7-7 7-7"
+					/>
+				</svg>
+			</button>
+			<h1 class="text-xl font-bold">Wyniki wyszukiwania</h1>
+		</div>		<div class="space-y-4 p-4">
 			{#each data.result as result}
 				<div class={`p-4 rounded-lg ${get_color(result)} border border-gray-300`}>
 					<div
