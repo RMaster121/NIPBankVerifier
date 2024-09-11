49d373d689ae7c8746e19e2d2044a5457a063d91
Rafal <rs.szczerba@hotmail.com>
2024-09-12 00:27:04 +0200

    Extracted EntityCard component for reusability and improved code organization

---
+++ src/lib/components/EntityCard.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { identifyNumber } from "$lib/utilities/input_type";
+    import { generatePDF } from "$lib/utilities/generate_pdf";
+    import type { Result } from "$lib/models/Result";
+
+    export let result: Result;
+
+    function is_veryfied(result: Result): boolean {
+		return result.company.bank_accounts.includes(result.search.bank_account);
+	}
+
+	function get_color(result: Result): string {
+		if (is_veryfied(result)) {
+			return 'bg-green-100';
+		} else {
+			return 'bg-red-100';
+		}
+	}
+
+	function get_icon(result: Result): string {
+		if (is_veryfied(result)) {
+			return '✔️';
+		}
+		return '❌';
+	}
+
+	function get_status(result: Result): string {
+		if (is_veryfied(result)) {
+			return 'Dane zgodne';
+		}
+		return 'Brak zgodności';
+	}
+
+	function bank_status(result: Result): string {
+		if (result.company.bank_accounts.length == 0) {
+			return 'Podmiot nie posiada kont bankowych.';
+		}
+		if (!is_veryfied(result)) {
+			return 'Podane konto nie jest powiązane z podmiotem.';
+		}
+		if (result.company.bank_accounts.length > 1) {
+			return (
+				'Podane konto jest jednym z ' +
+				result.company.bank_accounts.length +
+				' kont powiązanych z podmiotem.'
+			);
+		}
+		return 'Podane konto jest jedynym kontem powiązanym z podmiotem.';
+	}
+</script>
+
+<div class={`flex flex-row items-center p-4 rounded-lg py-1 ${get_color(result)} border border-gray-300`}>
+
+    <div class="flex flex-col w-2/5">
+        <p class="font-bold">
+            {identifyNumber(result.search.id_value)}
+            {result.search.id_value}
+        </p>
+        <p class="mb-1 mt-1">{result.company.name}</p>
+        <p>{result.company.address}</p>
+    </div>
+    <div class="mt-2 sm:mt-0 w-3/5">
+        <p class="text-lg font-semibold">Numer konta {result.search.bank_account}</p>
+        <p>{bank_status(result)}</p>
+    </div>
+    <div class="flex justify-end space-x-2 w-1/5">
+        <span>{get_icon(result)}</span>
+        <p>{get_status(result)}</p>
+    </div>
+    <div class="flex flex-col items-start sm:items-end text-blue-500 underline w-1/4">
+        <button on:click={() => generatePDF(result)}>Pobierz potwierdzenie</button>
+    </div>
+</div>
--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,61 +1,14 @@
 <script lang="ts">
 	import type { Result } from '$lib/models/Result';
-	import type { PageData } from './$types';
-	import { generatePDF } from '$lib/utilities/generate_pdf'
+	import EntityCard from '$lib/components/EntityCard.svelte';
 
-	function is_veryfied(result: Result): boolean {
-		return result.company.bank_accounts.includes(result.search.bank_account);
-	}
-
-	function get_color(result: Result): string {
-		if (is_veryfied(result)) {
-			return 'bg-green-100';
-		} else {
-			return 'bg-red-100';
-		}
-	}
-
-	function get_icon(result: Result): string {
-		if (is_veryfied(result)) {
-			return '✔️';
-		}
-		return '❌';
-	}
-
-	function get_status(result: Result): string {
-		if (is_veryfied(result)) {
-			return 'Dane zgodne';
-		}
-		return 'Brak zgodności';
-	}
-
-	function bank_status(result: Result): string {
-        if (result.company.bank_accounts.length == 0) {
-            return 'Podmiot nie posiada kont bankowych.';
-        }
-		if (!is_veryfied(result)) {
-			return 'Podane konto nie jest powiązane z podmiotem.';
-		}
-		if (result.company.bank_accounts.length > 1) {
-			return (
-				'Podane konto jest jednym z ' + result.company.bank_accounts.length + ' kont powiązanych z podmiotem.'
-			);
-		}
-        return 'Podane konto jest jedynym kontem powiązanym z podmiotem.';
-		
-	}
-
-	export let data: PageData;
+	export let data: { result: Result[] };
 </script>
 
-<div class="container mx-auto p-4">
-	<div class="bg-white shadow-md rounded-lg">
+<div class="container mx-auto p-4 bg-white shadow-md rounded-lg">
+	<div class="">
 		<div class="flex items-center p-4">
-			<button
-				class="mr-2"
-				on:click={() => history.back()}
-				aria-label="Back"
-			>
+			<button class="mr-2" on:click={() => history.back()} aria-label="Back">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-6 w-6 text-blue-500 hover:text-blue-700"
@@ -64,46 +17,14 @@
 					stroke="currentColor"
 					stroke-width="2"
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15 19l-7-7 7-7"
-					/>
+					<path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
 				</svg>
 			</button>
 			<h1 class="text-xl font-bold">Wyniki wyszukiwania</h1>
-		</div>		<div class="space-y-4 p-4">
+		</div>
+		<div class="space-y-4 p-4">
 			{#each data.result as result}
-				<div class={`p-4 rounded-lg ${get_color(result)} border border-gray-300`}>
-					<div
-						class="flex flex-col space-y-4 sm:space-y-0 sm:flex-row justify-between items-start sm:items-center"
-					>
-						<div class="flex-1">
-							<div class="flex flex-wrap sm:flex-nowrap sm:space-x-8">
-								<div>
-									<p class="font-bold">{result.search.id_type} {result.search.id_value}</p>
-									<p>{result.company.name}</p>
-									<p>{result.company.address}</p>
-								</div>
-								<div class="mt-2 sm:mt-0">
-									<p class="text-lg font-semibold">Numer konta {result.search.bank_account}</p>
-									<p>{bank_status(result)}</p>
-								</div>
-							</div>
-						</div>
-						<div
-							class="flex flex-col sm:flex-row items-start sm:items-center space-y-4 sm:space-y-0 sm:space-x-6 mt-4 sm:mt-0"
-						>
-							<div class="flex items-center space-x-2">
-								<span>{get_icon(result)}</span>
-								<p>{get_status(result)}</p>
-							</div>
-							<div class="flex flex-col items-start sm:items-end text-blue-500 underline">
-								<button on:click={() => generatePDF(result)}>Pobierz potwierdzenie</button>
-							</div>
-						</div>
-					</div>
-				</div>
+				<EntityCard {result} />
 			{/each}
 		</div>
 	</div>
