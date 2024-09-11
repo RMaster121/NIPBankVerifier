e2ff291748731f4db740dc257fb5740b060d08b8
Rafal <rs.szczerba@hotmail.com>
2024-09-12 00:48:32 +0200

    Added result formatting

--- src/lib/components/EntityCard.svelte
+++ src/lib/components/EntityCard.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	import { identifyNumber } from "$lib/utilities/input_type";
-    import { generatePDF } from "$lib/utilities/generate_pdf";
-    import type { Result } from "$lib/models/Result";
+	import { identifyNumber } from '$lib/utilities/input_type';
+	import { generatePDF } from '$lib/utilities/generate_pdf';
+	import type { Result } from '$lib/models/Result';
+	import { formatBankAccount, formatNIP } from '$lib/utilities/format';
 
-    export let result: Result;
+	export let result: Result;
 
-    function is_veryfied(result: Result): boolean {
+	function is_veryfied(result: Result): boolean {
 		return result.company.bank_accounts.includes(result.search.bank_account);
 	}
 
@@ -49,25 +50,28 @@
 	}
 </script>
 
-<div class={`flex flex-row items-center p-4 rounded-lg py-1 ${get_color(result)} border border-gray-300`}>
-
-    <div class="flex flex-col w-2/5">
-        <p class="font-bold">
-            {identifyNumber(result.search.id_value)}
-            {result.search.id_value}
-        </p>
-        <p class="mb-1 mt-1">{result.company.name}</p>
-        <p>{result.company.address}</p>
-    </div>
-    <div class="mt-2 sm:mt-0 w-3/5">
-        <p class="text-lg font-semibold">Numer konta {result.search.bank_account}</p>
-        <p>{bank_status(result)}</p>
-    </div>
-    <div class="flex justify-end space-x-2 w-1/5">
-        <span>{get_icon(result)}</span>
-        <p>{get_status(result)}</p>
-    </div>
-    <div class="flex flex-col items-start sm:items-end text-blue-500 underline w-1/4">
-        <button on:click={() => generatePDF(result)}>Pobierz potwierdzenie</button>
-    </div>
+<div
+	class={`flex flex-row items-center p-4 rounded-lg py-1 ${get_color(result)} border border-gray-300`}
+>
+	<div class="flex flex-col w-2/5">
+		<p class="font-bold">
+			{identifyNumber(result.search.id_value)}
+			{identifyNumber(result.search.id_value) == 'NIP'
+				? formatNIP(result.search.id_value)
+				: result.search.id_value}
+		</p>
+		<p class="mb-1 mt-1">{result.company.name}</p>
+		<p>{result.company.address}</p>
+	</div>
+	<div class="mt-2 sm:mt-0 w-3/5">
+		<p class="text-lg font-semibold">Numer konta {formatBankAccount(result.search.bank_account)}</p>
+		<p>{bank_status(result)}</p>
+	</div>
+	<div class="flex justify-end space-x-2 w-1/5">
+		<span>{get_icon(result)}</span>
+		<p>{get_status(result)}</p>
+	</div>
+	<div class="flex flex-col items-start sm:items-end text-blue-500 underline w-1/4">
+		<button on:click={() => generatePDF(result)}>Pobierz potwierdzenie</button>
+	</div>
 </div>
