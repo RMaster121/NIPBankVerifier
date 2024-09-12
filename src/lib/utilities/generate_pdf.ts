a8dd9e45718912a61930ac12531c593e6e4cb01f
Rafal <rs.szczerba@hotmail.com>
2024-09-12 18:15:54 +0200

    Repaired font and added multipage

--- src/lib/utilities/generate_pdf.ts
+++ src/lib/utilities/generate_pdf.ts
@@ -2,17 +2,20 @@ import type { Result } from './../models/Result';
 import jsPDF from 'jspdf';
 import { formatBankAccount } from './format';
 import { callAddFont } from '$lib/Roboto-Regular-normal';
-
+import { callAddFontBold } from '$lib/Roboto-Bold-normal';
 
 export function generatePDF(result: Result): void {
 	jsPDF.API.events.push(['addFonts', callAddFont]);
+	jsPDF.API.events.push(['addFonts', callAddFontBold]);
 
 	let font = 'Roboto-Regular';
+	let fontBold = 'Roboto-Bold';
+
 
 	const doc = new jsPDF();
 
 	// Set the title
-	doc.setFont(font, 'bold');
+	doc.setFont(fontBold, 'normal');
 	doc.setFontSize(20);
 	doc.text('Raport - weryfikacja danych podmiotu', 20, 20);
 
@@ -27,7 +30,7 @@ export function generatePDF(result: Result): void {
 
 	// Add full name
 	doc.setFontSize(16);
-	doc.setFont(font, 'bold');
+	doc.setFont(fontBold, 'normal');
 	doc.text('Nazwa podmiotu', 20, 43);
 	doc.setFont(font, 'normal');
 	doc.setFontSize(18);
@@ -39,7 +42,7 @@ export function generatePDF(result: Result): void {
 
 	// Add NIP, REGON, and address header
 	doc.setFontSize(14);
-	doc.setFont(font, 'bold');
+	doc.setFont(fontBold, 'normal');
 	doc.text('NIP', 20, 68);
 	doc.text('REGON', 60, 68);
 	doc.text('Adres siedziby', 110, 68);
@@ -55,7 +58,7 @@ export function generatePDF(result: Result): void {
 	doc.line(20, 83, 190, 83);
 
 	// Add "Konta bankowe podmiotu" header
-	doc.setFont(font, 'bold');
+	doc.setFont(fontBold, 'normal');
 	doc.text('Konta bankowe podmiotu', 20, 93);
 
 	// Add bank accounts
@@ -63,16 +66,37 @@ export function generatePDF(result: Result): void {
 	doc.setFontSize(14);
 
 	// Dynamically add each bank account
-	result.company.bank_accounts.forEach((account, index) => {
-		const yPos = 103 + index * 10; // Start at y = 103 and add 10 for each account
+	let yIndex = 103;
+	for (let i in result.company.bank_accounts) {
+		const account = result.company.bank_accounts[i];
 		if (account == result.search.bank_account && result.company.bank_accounts.length > 1) {
-			doc.setFont(font, 'bold');
-			doc.text(`- ${formatBankAccount(account)}`, 20, yPos);
+			doc.setFont(fontBold, 'normal');
+			doc.text(`- ${formatBankAccount(account)}`, 20, yIndex);
 			doc.setFont(font, 'normal');
+		} else {
+			doc.text(`- ${formatBankAccount(account)}`, 20, yIndex);
 		}
-		doc.text(`- ${formatBankAccount(account)}`, 20, yPos);
+
+		yIndex += 10;
+		if (yIndex > doc.internal.pageSize.height - 20) {
+			doc.addPage();
+			yIndex = 20;
+		}
+	}
+
+	for (let i in doc.internal.pages) {
+		doc.setPage(Number(i));
+		doc.setFontSize(12);
+		doc.text('Strona: ' + i + '/' + (doc.internal.pages.length - 1), 
+			doc.internal.pageSize.width/2, 
+		doc.internal.pageSize.height - 10, { align: 'center' });
+	}
+
+	//Set the filename
+	doc.setProperties({
+		title: `Raport-${new Date().toLocaleDateString().split('.').join('-')}-${result.company.nip_value}.pdf`
 	});
 
 	// Save the PDF
-	doc.save(`Raport-${new Date().toLocaleDateString().split('.').join('-')}-${result.company.nip_value}.pdf`);
+	doc.output('dataurlnewwindow');
 }
